3f5d3c21cc7b7c05bcd4012033bc836635f3a505
Valeria Marchenko <marcenkolera60@gmail.com>
2020-09-30 03:01:55 +0700

    начало положено

---
+++ 3laba/3laba/3laba.cpp
@@ -0,0 +1,43 @@
+﻿// 3laba.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+#include <iostream>
+#include <Windows.h>
+#include <string>
+#include <conio.h>
+#include <malloc.h>
+
+using namespace std;
+
+
+
+class wallet
+{
+private:
+	float rubles;
+	float dollars;
+	float euros;
+public:
+	wallet();
+	wallet(float rubles, float dollars, float euros);
+	~wallet();
+	void set_wallet(float rubles, float dollars, float euros);
+	void print_wallet();
+	void add_wallet(wallet now);
+	void convert(wallet now);
+	void purchase(wallet now);
+};
+
+
+wallet::wallet()
+{
+	rubles = 0;
+	dollars = 0;
+	euros = 0;
+}
+
+
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
